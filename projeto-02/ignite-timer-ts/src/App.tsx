33f0f5fcde6220630626810735506af2a99958eb
Mateus Paulo <mateusdev1337@gmail.com>
2023-04-15 10:40:37 -0300

    tipagem de temas

--- projeto-02/ignite-timer-ts/src/App.tsx
+++ projeto-02/ignite-timer-ts/src/App.tsx
@@ -1,13 +1,14 @@
 import { Button } from "./components/Button"
-
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/theme/default"
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="danger" />
       <Button variant="success" />
-    </>
+    </ThemeProvider>
   )
 }
 
