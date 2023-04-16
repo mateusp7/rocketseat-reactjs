1ec3c38deb17b4626f33b9e6ca878e07beff146c
Mateus Paulo <mateusdev1337@gmail.com>
2023-04-16 16:41:39 -0300

    create layout with outlet react router dom

--- projeto-02/ignite-timer-ts/src/styles/global.ts
+++ projeto-02/ignite-timer-ts/src/styles/global.ts
@@ -19,7 +19,7 @@ export const GlobalStyle = createGlobalStyle`
 
   body, input, textarea, button {
     font-family: 'Roboto', sans-serif;
-    font-weight: 400;
+    font-weight: 600;
     font-size: 1rem;
   }
   
