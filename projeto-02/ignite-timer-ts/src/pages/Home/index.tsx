4f51ec908beca76d0b55d13f1561172c7ebe2a27
Mateus Paulo <mateusdev1337@gmail.com>
2023-04-17 18:39:54 -0300

    create a new cycle

--- projeto-02/ignite-timer-ts/src/pages/Home/index.tsx
+++ projeto-02/ignite-timer-ts/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import * as zod from 'zod'
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
@@ -23,7 +23,16 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
+
 const Home = () => {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -31,12 +40,26 @@ const Home = () => {
       minutesAmount: 0,
     },
   })
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  console.log(activeCycle)
+
   const task = watch('task')
   const isSubmitDisabled = !task
 
   function handleCreateNewCycle(data: NewCycleFormData) {
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+
     reset()
-    console.log(data)
   }
 
   return (
